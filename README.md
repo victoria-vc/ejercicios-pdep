42f15cac0b377036ab35c1b974e63ee0bf9c9d1f
victoria-vc <vcastagno@frba.utn.edu.ar>
2026-04-20 20:57:53 -0300

    Primer commit

---
+++ README.md
@@ -0,0 +1,112 @@
+# Ejercicios de Haskell
+(Hasta el 14 son de la clase)
+## Ejercicio 1: Tiempo de viaje
+
+Una combi tarda 1.5 horas en recorrer 100 km.
+
+Dada una distancia, obtener el tiempo del viaje.
+
+---
+
+## Ejercicio 2: Costo por tiempo
+
+El costo de un viaje es:
+- $5000 base
+- $200 por minuto
+
+Dada una cantidad de horas, obtener el costo de un viaje.
+
+---
+
+## Ejercicio 3: Costo por distancia
+
+Teniendo en cuenta que:
+- La combi tarda 1.5 horas cada 100 km
+- El costo es $5000 base + $200 por minuto
+
+Obtener el costo de un viaje dada una distancia.
+
+---
+
+## Ejercicio 4: Longitud de un string
+
+Dado un String, obtener su longitud.
+
+---
+
+## Ejercicio 5: Número par
+
+Dado un número, indicar si es par.
+
+---
+
+## Ejercicio 6: Número impar
+
+Dado un número, indicar si es impar.
+
+---
+
+## Ejercicio 7: Aplicar función
+
+Definir una función que:
+- reciba una función
+- reciba un valor
+
+y aplique la función al valor.
+
+---
+
+## Ejercicio 8: Sumar dos
+
+Definir una función que sume 2 a un número.
+
+---
+
+## Ejercicio 9: Palabra más corta
+
+Dadas dos palabras, devolver la longitud de la palabra más corta.
+
+---
+
+## Ejercicio 10: Empieza con A
+
+Dada una palabra, indicar si empieza con la letra 'A'.
+
+---
+
+## Ejercicio 11: Composición de funciones
+
+Definir la función de composición:
+
+(f . g)(x) = f(g(x))
+
+---
+
+## Ejercicio 12: Peso de un pino
+
+En una plantación de pinos:
+
+- Cada árbol tiene una altura en metros
+- El peso se calcula así:
+  - 3 kg por cada centímetro hasta 3 metros
+  - 2 kg por cada centímetro por encima de 3 metros
+
+Definir la función `pesoPino`, que recibe la altura y devuelve el peso.
+
+---
+
+## Ejercicio 13: Peso útil
+
+Una fábrica de muebles acepta árboles que pesen entre:
+
+- 400 kg y 1000 kg
+
+Definir la función `esPesoUtil`, que indique si un peso es útil.
+
+---
+
+## Ejercicio 14: Pino útil
+
+Definir la función `sirvePino`, que:
+- recibe la altura de un pino
+- indica si sirve para la fábrica
---
+++ src/Library.hs
@@ -0,0 +1,104 @@
+module Library where
+import PdePreludat
+
+doble :: Number -> Number
+doble numero = numero + numero
+
+
+-- Ejercicio 1
+obtenerTiempoViaje :: Number -> Number
+obtenerTiempoViaje distancia = distancia * 1.5 / 100
+
+
+-- Ejercicio 2
+obtenerCostoHoras :: Number -> Number
+obtenerCostoHoras horas = horasAMinutos horas * 200 + 5000
+
+horasAMinutos :: Number -> Number
+horasAMinutos horas = horas * 60
+
+
+-- Ejercicio 3
+obtenerCostoViaje :: Number -> Number
+obtenerCostoViaje distancia = (obtenerCostoHoras . obtenerTiempoViaje) distancia
+
+
+-- Ejercicio 4
+longitud :: String -> Number
+longitud string = length string
+
+
+-- Ejercicio 5
+par :: Number -> Bool
+par numero = even numero
+
+
+-- Ejercicio 6
+impar :: Number -> Bool
+impar = odd
+
+imparV2 :: Number -> Bool
+imparV2 numero = (negar . par) numero
+
+imparV2V2 :: Number -> Bool
+imparV2V2 numero = (.) negar par numero
+
+imparV3 :: Number -> Bool
+imparV3 numero = negar . par $ numero
+
+
+-- Ejercicio 7
+pesos :: (algo -> otroAlgo) -> algo -> otroAlgo
+pesos funcion parametro = funcion parametro
+
+negar :: Bool -> Bool
+negar bool = not bool
+
+
+-- Ejercicio 8
+sumaSiempreDos :: Number -> Number
+sumaSiempreDos nro = nro + 2
+
+sumaSiempreDos2 :: Number -> Number
+sumaSiempreDos2 nro = (+) nro 2
+
+cinco :: Number
+cinco = 5
+
+sumarDos :: Number -> Number
+sumarDos numero = numero + 2
+
+
+-- Ejercicio 9
+minimoEntreDosPalabras :: String -> String -> Number
+minimoEntreDosPalabras palabra1 palabra2 = min (length palabra1) (length palabra2)
+
+
+-- Ejercicio 10
+palabraEmpiezaConA :: String -> Bool
+palabraEmpiezaConA palabra = head palabra == 'A'
+
+
+-- Ejercicio 11
+composicion :: (b -> c) -> (a -> b) -> a -> c
+composicion f g x = f (g x)
+
+
+-- Ejercicio 12, 13 y 14
+pesoPino :: Number -> Number
+pesoPino altura = pesoBasePino altura + pesoTroncoPino altura
+
+metrosACentimetros :: Number -> Number
+metrosACentimetros metros = metros * 100
+
+pesoBasePino :: Number -> Number
+pesoBasePino altura = (metrosACentimetros . alturaBase) altura * 3
+
+pesoTroncoPino :: Number -> Number
+pesoTroncoPino altura = (metrosACentimetros . alturaTronco) altura * 2
+
+alturaBase :: Number -> Number
+alturaBase altura = min altura 3
+
+alturaTronco :: Number -> Number
+alturaTronco altura = max (altura - 3) 0
